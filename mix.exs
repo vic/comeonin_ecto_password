14c157a15992368b4260764ad3c3987ddb3bf9e7
Victor Borja <vborja@apache.org>
2016-04-16 15:14:57 -0500

    fix version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ComeoninEctoPassword.Mixfile do
 
   def project do
     [app: :comeonin_ecto_password,
-     version: "0.0.3",
+     version: "2.0.0-rc.0",
      elixir: "~> 1.1",
      description: description,
      package: package,
